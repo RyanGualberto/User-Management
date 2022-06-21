54d28f1a35f127a31252a6a3efffe5d2b36ef7e3
Ryan Gualberto <RyanOliveiraGualberto@gmail.com>
2022-06-21 12:51:42 -0300

    aula 46: add: validate form

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,8 +57,16 @@ class UserController {
 
     getValues() {
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+                return false;
+            }
+
             if (field.name == "gender") {
                 if (field.checked) {
                     user[field.name] = field.value
@@ -72,6 +80,10 @@ class UserController {
             }
             // console.log(field.name, field.id, field.value);
         });
+
+        if(!isValid){
+            return false;
+        }
 
         return new User(
             user.name,
